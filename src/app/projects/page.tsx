b4af9812152c5eace6a3950183e1d2cfea3439a0
Rahul Pawar <rahulpwr377@gmail.com>
2025-07-16 19:51:10 +0530

    fit : Projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,64 @@
-export default function Projects(){
-    return(
+export default function Projects() {
+    return (
         <>
-            <h1>Projects</h1>
+            <h1>My Projects</h1>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Sr No.</th>
+                        <th>Project Name</th>
+                    </tr>
+                    <tr>
+                        <td>1.</td>
+                        <td>
+                            <a href="https://rahulpawar140.github.io/Simple_calculator/">Calculator</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2.</td>
+                        <td>
+                            <a href="https://rahulpawar140.github.io/Weather_API/">Weather API</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3.</td>
+                        <td>
+                            <a href="https://rahulpawar140.github.io/Tic_Tac_Toe/">Tic Tac Toe</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>4.</td>
+                        <td>
+                            <a href="https://rahulpawar140.github.io/Random_User_API/">Random User API</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>5.</td>
+                        <td>
+                            <a href="https://rahulpawar140.github.io/Shopping_list/">Shopping List</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>6.</td>
+                        <td>
+                            <a href="https://rahulpawar140.github.io/Expense_Tracker/">Expense Tracker</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>7.</td>
+                        <td>
+                            <a href="https://rahulpawar140.github.io/test/">Drone Web using Bootstrap</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>8.</td>
+                        <td>
+                            <a href="https://rahulpawar140.github.io/Memory_Game/">Memory Game</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </>
     )
 }
